4c0ae24a2b704bc60e5d93a66d851072dfe9f14b
kvnxiao <xlzqwerty1@gmail.com>
2020-03-07 17:46:49 -0500

    Trim hwids

--- src/hwid.ts
+++ src/hwid.ts
@@ -4,7 +4,7 @@ function getMac(): string {
   try {
     const m = execSync("cat /sys/class/net/eth0/address", { encoding: "utf-8" })
     if (m.length > 0) {
-      return m
+      return m.trim()
     }
   } catch (err) {
     console.log("Could not get MAC address of eth0 interface.")
@@ -16,7 +16,7 @@ function getSerial(): string {
   try {
     const s = execSync("cat /proc/cpuinfo | grep Serial | cut -d ' ' -f 2", { encoding: "utf-8" })
     if (s.length > 0) {
-      return s
+      return s.trim()
     }
   } catch (err) {
     console.log("Could not get serial number of device.")

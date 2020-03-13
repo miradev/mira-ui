a1048c609098636b7d2a81667298cf05903a8439
kvnxiao <xlzqwerty1@gmail.com>
2020-03-13 01:47:31 -0400

    Use /#/

--- src/renderer/setup/setup.ts
+++ src/renderer/setup/setup.ts
@@ -2,7 +2,7 @@
   const deviceId = window.hwid.mac + "-" + window.hwid.serial
   const didSpan = document.getElementById("did")! as HTMLSpanElement
   const urlSpan = document.getElementById("url")! as HTMLSpanElement
-  const url = `${window.serverConfig.serverUrl}:${window.serverConfig.frontendPort}/`
+  const url = `${window.serverConfig.serverUrl}:${window.serverConfig.frontendPort}/#/`
   didSpan.innerText = deviceId
   urlSpan.innerText = url
 

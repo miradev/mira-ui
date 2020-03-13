793e409e39d3e22c5250276c4487b7b2bdd194c0
kvnxiao <xlzqwerty1@gmail.com>
2020-03-13 01:36:25 -0400

    Use query param

--- src/renderer/setup/setup.ts
+++ src/renderer/setup/setup.ts
@@ -2,13 +2,13 @@
   const deviceId = window.hwid.mac + "-" + window.hwid.serial
   const didSpan = document.getElementById("did")! as HTMLSpanElement
   const urlSpan = document.getElementById("url")! as HTMLSpanElement
-  const url = `${window.serverConfig.serverUrl}:${window.serverConfig.frontendPort}/qr`
+  const url = `${window.serverConfig.serverUrl}:${window.serverConfig.frontendPort}/`
   didSpan.innerText = deviceId
   urlSpan.innerText = url
 
   // @ts-ignore
   new QRCode(document.getElementById("qrcode"), {
-    text: encodeURI(`${window.serverConfig.protocol}://${url}/${deviceId}`),
+    text: encodeURI(`${window.serverConfig.protocol}://${url}/qr?deviceId=${deviceId}`),
     width: 256,
     height: 256,
     // @ts-ignore
